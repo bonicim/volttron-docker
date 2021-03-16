339e51d11d5d2123fb801f06006ba7d52bcfd6f9
Mark Bonicillo <mark.bonicillo@pnnl.gov>
2021-03-16 11:32:10 -0700

    Add container restarts

---
+++ run-restart-containers.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+agentlist=(forwarderagent-5.1 listeneragent-3.3)
+containerlist=(fwd0 fwd1 fwd2 fwd3 fwd4 fwd5 fwd6 fwd7 fwd8 fwd9)
+
+for i in "${containerlist[@]}"
+do
+  docker stop "${i}"
+  docker start "${i}"
+	docker exec -u volttron "${i}" /home/volttron/.local/bin/volttron -vv -l volt.log &
+  echo "${agentlist[@]}" | xargs -n1 docker exec -u volttron "${i}" /home/volttron/.local/bin/vctl restart
+  docker exec -u volttron "${i}" /home/volttron/.local/bin/vctl status
+done
+
