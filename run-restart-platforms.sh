ec9c51fad30d5afe3b8cac69b03e65f67509794c
Mark Bonicillo <mark.bonicillo@pnnl.gov>
2021-03-16 11:52:30 -0700

    Update restart platforms

--- run-restart-platforms.sh
+++ run-restart-platforms.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+set -o xtrace   # prints every expression before executing it
 agentlist=(forwarderagent-5.1 listeneragent-3.3)
 containerlist=(fwd0 fwd1 fwd2 fwd3 fwd4 fwd5 fwd6 fwd7 fwd8 fwd9)
 
@@ -21,5 +21,5 @@ done
 # Check status of agents
 for i in "${containerlist[@]}"
 do
-	echo "${agentlist[@]}" | xargs -n1 docker exec -u volttron "${i}" /home/volttron/.local/bin/vctl status
+	docker exec -u volttron "${i}" /home/volttron/.local/bin/vctl status
 done
