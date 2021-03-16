373ba720bbf32e18db0a826fb1821a46cb5615e2
Mark Bonicillo <mark.bonicillo@pnnl.gov>
2021-03-16 11:21:00 -0700

    Add comments to script

--- run-restart-platforms.sh
+++ run-restart-platforms.sh
@@ -3,6 +3,7 @@
 agentlist=(forwarderagent-5.1 listeneragent-3.3)
 containerlist=(fwd0 fwd1 fwd2 fwd3 fwd4 fwd5 fwd6 fwd7 fwd8 fwd9)
 
+# shutdown, then restart the platform
 for i in "${containerlist[@]}"
 do
 	docker exec -u volttron "${i}" /home/volttron/.local/bin/vctl shutdown --platform
@@ -10,12 +11,14 @@ do
 done
 
 
+# Restart all agents
 for i in "${containerlist[@]}"
 do
 	echo "${agentlist[@]}" | xargs -n1 docker exec -u volttron "${i}" /home/volttron/.local/bin/vctl restart
 done
 
 
+# Check status of agents
 for i in "${containerlist[@]}"
 do
 	echo "${agentlist[@]}" | xargs -n1 docker exec -u volttron "${i}" /home/volttron/.local/bin/vctl status
